f76a2c30567f1c4ee2a12675496642389d5aec21
Motts <mnd.matias.diaz@gmail.com>
2017-07-14 16:55:03 -0300

    Slider Instrucciones

---
+++ Republica de los Niños/Assets/Scripts/AppController.cs
@@ -0,0 +1,31 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class AppController : MonoBehaviour {
+
+    #region Atributos
+    [SerializeField]
+    GameObject InstruccionesCanvas;
+    [SerializeField]
+    GameObject mainCamera;
+    [SerializeField]
+    GameObject ARCamera;
+    #endregion
+
+    public void DesactivarInstrucciones()
+    {
+        mainCamera.SetActive(false);
+        InstruccionesCanvas.SetActive(false);
+        ARCamera.SetActive(true);
+    }
+
+    public void ActivarInstrucciones()
+    {
+        ARCamera.SetActive(false);
+        InstruccionesCanvas.SetActive(true);
+        mainCamera.SetActive(true);
+    }
+
+
+}
---
+++ Republica de los Niños/Assets/Scripts/TargetBehaviour.cs
@@ -0,0 +1,40 @@
+﻿/*==============================================================================
+Copyright (c) 2010-2014 Qualcomm Connected Experiences, Inc.
+All Rights Reserved.
+Confidential and Proprietary - Protected under copyright and other laws.
+==============================================================================*/
+
+using UnityEngine;
+
+namespace Vuforia
+{
+    /// <summary>
+    /// A utility behaviour to disable rendering of a game object at run time.
+    /// </summary>
+    public class TargetBehaviour : TurnOffAbstractBehaviour
+    {
+
+        #region UNITY_MONOBEHAVIOUR_METHODS
+
+        void Awake()
+        {
+            if (VuforiaRuntimeUtilities.IsVuforiaEnabled())
+            {
+                // We remove the mesh components at run-time only, but keep them for
+                // visualization when running in the editor:
+                MeshRenderer targetMeshRenderer = this.GetComponent<MeshRenderer>();
+                Destroy(targetMeshRenderer);
+                MeshFilter targetMesh = this.GetComponent<MeshFilter>();
+                Destroy(targetMesh);
+                print("Targetoff" + this.name);
+            }
+            else
+            {
+                print("TargetOn: " + this.name);
+            }
+        }
+
+        #endregion // UNITY_MONOBEHAVIOUR_METHODS
+
+    }
+}
